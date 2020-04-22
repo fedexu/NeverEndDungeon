a15b0df108d100a772bbda9caecc849df82be1f2
fedexu <federico.xu125@gmail.com>
2020-04-22 16:18:02 +0200

    basic movement of knight

---
+++ Assets/scripts/BasicMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private bool facingRight = true; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
+        transform.position = transform.position + horizontal * Time.deltaTime;
+
+        flip();
+    }
+
+    private void flip(){
+        if (Input.GetAxis("Horizontal") > 0 && !facingRight || Input.GetAxis("Horizontal") < 0 && facingRight){
+            facingRight = !facingRight;
+            Vector3 scale = transform.localScale;
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+    }
+
+}
