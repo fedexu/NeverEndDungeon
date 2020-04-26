043b6fc4deafd633430e38f46af8d1bdbc582e26
fedexu <federico.xu125@gmail.com>
2020-04-26 17:52:31 +0200

    added cosmetics to wall

--- Assets/scripts/roomBasic/WallGenerator.cs
+++ Assets/scripts/roomBasic/WallGenerator.cs
@@ -9,26 +9,26 @@ public class WallGenerator : MonoBehaviour
     public Sprite[] guards;
     public Sprite[] sideGuards;
     public Sprite[] backGuards;
+    public Sprite[] cosmetics;
+    public Sprite[] cosmeticsCollision;
+    public Sprite[] column;
 
     // 100 for 1% => (1) , 1000 for 0.1% => (1) && 1% => (10) ecc..
     private static int percentagePrecision = 1000;
-    private static int wallPercentage = 400;
+    private static int wallPercentage = 200;
     private static int guardPercentage = 500;
+    private static int cosmeticPercentage = 100;
+    private static int cosmeticCollisionPercentage = 50;
+    private static int columnPercentage = 50;
 
     // Start is called before the first frame update
     void Start()
     {
-        // int clickPercentage = 60;
-        // if (Random.Range(0, percentagePrecision) < clickPercentage)
-        // {
-        //     //do 70% times
-        // }
-
         string component = transform.gameObject.tag;
         switch (component)
         {
             case "Wall_1_A":
-                randomSpriteGenerator("wall", walls, wallPercentage);
+                addWallCosmetic();
                 randomSpriteGenerator("guard", guards, guardPercentage);
                 break;
             case "Wall_2_A":
@@ -44,7 +44,7 @@ public class WallGenerator : MonoBehaviour
         }
     }
 
-    private void randomSpriteGenerator(string elementToFind, Sprite[] spritesToChoose, int chancePercentage)
+    private bool randomSpriteGenerator(string elementToFind, Sprite[] spritesToChoose, int chancePercentage)
     {
         SpriteRenderer spriteRenderer = transform.Find(elementToFind).transform.gameObject.GetComponent<SpriteRenderer>();
         // default the first occurrence of the sprites
@@ -52,7 +52,36 @@ public class WallGenerator : MonoBehaviour
 
         if (Random.Range(0, percentagePrecision) <= chancePercentage)
         {
+            //do "chancePercentage" times
             spriteRenderer.sprite = spritesToChoose[Random.Range(1, spritesToChoose.Length)];
+            return true;
+        }
+        return false;
+    }
+
+    private void addWallCosmetic()
+    {
+        if (!randomSpriteGenerator("wall", walls, wallPercentage))
+        {
+            //add cosmetic object only if simple wall is picked
+            if (!randomSpriteGenerator("cosmetic", cosmetics, cosmeticPercentage))
+            {
+                if (!randomSpriteGenerator("cosmeticCollision", cosmeticsCollision, cosmeticCollisionPercentage))
+                {
+                    if (randomSpriteGenerator("column", column, columnPercentage))
+                    {
+                        transform.Find("column").transform.gameObject.SetActive(true);
+                    }
+                }
+                else
+                {
+                    transform.Find("cosmeticCollision").transform.gameObject.SetActive(true);
+                }
+            }
+            else
+            {
+                transform.Find("cosmetic").transform.gameObject.SetActive(true);
+            }
         }
     }
 
